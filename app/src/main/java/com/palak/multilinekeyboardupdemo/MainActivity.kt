223139ccab176e7f1c5e6d0a6aae39fdd7364e47
palaksdarji <palaksdarji@gmail.com>
2019-12-29 12:44:27 +0530

    cleanup

--- app/src/main/java/com/palak/multilinekeyboardupdemo/MainActivity.kt
+++ app/src/main/java/com/palak/multilinekeyboardupdemo/MainActivity.kt
@@ -1,12 +1,7 @@
 package com.palak.multilinekeyboardupdemo
 
-import android.graphics.Rect
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -20,20 +15,6 @@ class MainActivity : AppCompatActivity(){
 
         keyboardHeightProvider = KeyboardHeightProvider(this)
         root.post { keyboardHeightProvider.start(nestedScroll, root, viewSpace, 0, btnSubmit.height) }
-    }
-
-
-    override fun onPause() {
-        super.onPause()
-        //Dont delete this
-        //keyboardHeightProvider.setKeyboardHeightObserver(null)
-    }
-
-    override fun onResume() {
-        super.onResume()
-       // keyboardHeightProvider.setKeyboardHeightObserver(this)
-
-
     }
 
     override fun onDestroy() {
